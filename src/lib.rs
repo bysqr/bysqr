e78e087ca7abc703c3a0b603d8cb2aea1eee4754
Peter Stovka <stovka.peter@gmail.com>
2025-01-18 11:26:55 +0100

    wasm compile

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
+use wasm_bindgen::prelude::wasm_bindgen;
+use crate::models::Pay;
+
 pub mod encoder;
 pub mod models;
 pub mod qr;
+
+#[wasm_bindgen]
+pub fn encode_pay_xml_to_svg(xml: &str) -> String {
+    let pay: Pay = quick_xml::de::from_str(&xml).expect("unable to decode XML file");
+    let encoded = encoder::encode(&pay);
+    let svg = qr::create_pay_svg(&encoded, qr::Theme::default());
+    String::from_utf8(svg).unwrap()
+}
